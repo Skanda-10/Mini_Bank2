d525ffa690be07a19ec625fb52ced483de7645bf
Skanda.TL <skandathoguluva@gmail.com>
2025-10-24 18:52:35 +0530

    Account-Service Minor changes

--- Account-Server/src/main/java/com/ash/Account_Service/AccountServiceApplication.java
+++ Account-Server/src/main/java/com/ash/Account_Service/AccountServiceApplication.java
@@ -2,12 +2,22 @@ package com.ash.Account_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
